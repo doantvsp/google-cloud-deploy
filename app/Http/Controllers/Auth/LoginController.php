67c1064e3c589b58bf4d43070c01d3668a8c6b68
DoanTV <doantv@hybrid-technologies.vn>
2022-05-05 09:55:03 +0700

    check convension

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,8 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+    public function test(){
+        $A = TRUE;
+        die('hello world....hello');
+    }
 }
