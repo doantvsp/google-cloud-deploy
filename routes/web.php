dce4f1f3990a05b5b2bfe6999ba305a1dc3e56c2
DoanTV <doantvsp@gmail.com>
2024-04-06 13:35:18 +0700

    test cron job

--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::post('/run-schedule', function () {
+    Artisan::call('schedule:cloud');
+});
