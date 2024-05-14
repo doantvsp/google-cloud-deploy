9dfc44b72fe950e154a76d82adb3d9e5c3c376d7
DoanTV <doantv@hybrid-technologies.vn>
2024-05-14 16:45:27 +0700

    add ga4 to website

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,15 +63,17 @@
             }
         </style>
 
+        
         <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-NLPWPVKKMC"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-346LMQJSCT"></script>
         <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', 'G-NLPWPVKKMC');
+        gtag('config', 'G-346LMQJSCT');
         </script>
+
 
     </head>
     <body>
